4d64e99c0d1e4c3c871da4031eac794db2742adc
Igor Guilherme Oleiro Patrocinio <2241551@my.ipleiria.pt>
2025-11-18 19:01:52 +0000

    RBAC frontofice
    user create corrigido

--- backend/modules/api/controllers/AnimalController.php
+++ backend/modules/api/controllers/AnimalController.php
@@ -11,4 +11,66 @@ class AnimalController extends ActiveController
 {
     public $modelClass = 'common\models\Animal';
 
+    public function actionAnimalsComplete()
+    {
+    $animalsComplete= new$this->modelClass;
+    $recs= $animalsComplete::find()->all();
+    return['animalsComplete' =>$recs];
+    }
+
+//    public function actionNomes()
+//    {
+//    $pratomodel= new$this->modelClass;
+//    $recs= $pratomodel::find()->select(['nome'])->all();
+//    return$recs;
+//    }
+
+//    public function actionPreco($id)
+//    {
+//    $pratomodel= new$this->modelClass;
+//    $rec= $pratomodel::find()->select(['preco'])
+//    ->where(['id' => $id])->one(); //objeto json
+//    return$rec;
+//    }
+
+//public function actionPrecopornome($nomeprato)
+//{
+//$pratomodel= new$this->modelClass;
+//$recs= $pratomodel::find()->select(['preco'])
+//->where(['nome' => $nomeprato])->all(); //array
+//return$recs;
+//}
+
+//public function actionDelpornome($nomeprato)
+//{
+//$climodel= new$this->modelClass;
+//$recs= $climodel::deleteAll(['nome' => $nomeprato]);
+//return$recs;
+//}
+
+//public function actionPutprecopornome($nomeprato)
+//{
+//$novo_preco=\Yii::$app->request->post('preco');
+//$climodel= new$this->modelClass;
+//$ret= $climodel::findOne(['nome' => $nomeprato]);
+//if($ret) {
+//$ret->preco= $novo_preco;
+//$ret->save();
+//}
+//else{
+//    thrownew\yii\web\NotFoundHttpException("Nome de prato não existe");
+//}
+//}
+
+//public function actionPostpratovazio()
+//{
+//$pratomodel= new$this->modelClass;
+//$pratomodel->id=0; //é autonumber!
+//$pratomodel->nome=' ';
+//$pratomodel->descricao=' ';
+//$pratomodel->preco=0;
+//$pratomodel->disponivel=0;
+//$pratomodel->save();
+//return$pratomodel;
+//}
 }
