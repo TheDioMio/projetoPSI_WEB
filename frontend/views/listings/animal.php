8dcf07de118d6d5b221cac83296bb599a7de623c
mariopina <2241534@my.ipleiria.pt>
2025-11-29 14:26:42 +0000

    Lista de Anuncios
    Criei a paginação da lista de anuncios

--- frontend/views/listings/animal.php
+++ frontend/views/listings/animal.php
@@ -100,21 +100,21 @@ $posterUrl = Yii::getAlias('@web/video/' . $escolhaAleatoria['posterFile']);
                 </div>
             <?php endforeach; ?> <div class="col-12">
                 <nav aria-label="Page navigation">
-                    <ul class="pagination pagination-lg m-0">
-                        <li class="page-item disabled">
-                            <a class="page-link rounded-0" href="#" aria-label="Previous">
-                                <span aria-hidden="true"><i class="bi bi-arrow-left"></i></span>
-                            </a>
-                        </li>
-                        <li class="page-item active"><a class="page-link" href="#">1</a></li>
-                        <li class="page-item"><a class="page-link" href="#">2</a></li>
-                        <li class="page-item"><a class="page-link" href="#">3</a></li>
-                        <li class="page-item">
-                            <a class="page-link rounded-0" href="#" aria-label="Next">
-                                <span aria-hidden="true"><i class="bi bi-arrow-right"></i></span>
-                            </a>
-                        </li>
-                    </ul>
+
+                    <?= \yii\widgets\LinkPager::widget([
+                        'pagination' => $provider->pagination,
+
+                        // Classes gerais
+                        'options' => [
+                            'class' => 'pagination pagination-lg m-0',
+                        ],
+                        'linkContainerOptions' => ['class' => 'page-item'],
+                        'linkOptions' => ['class' => 'page-link'],
+
+                        'prevPageLabel' => '<i class="bi bi-arrow-left"></i>',
+                        'nextPageLabel' => '<i class="bi bi-arrow-right"></i>',
+                    ]) ?>
+
                 </nav>
             </div>
         </div>
