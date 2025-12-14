932dc5610ee2e121a57c3608fc77bb846bc7a897
mariopina <2241534@my.ipleiria.pt>
2025-12-14 12:43:48 +0000

    Mosquitto
    Implementação começada

---
+++ backend/mosquitto/MosquittoCatcher.php
@@ -0,0 +1,21 @@
+<?php
+
+class MosquittoCatcher
+{
+    public static function makePublish(string $canal, string $msg)
+    {
+        $server = "127.0.0.1";
+        $port = 1883;
+        $username = ""; // set your username
+        $password = ""; // set your password
+        $client_id = "phpMQTT-publisher"; // unique!
+        $mqtt = new phpMQTT($server, $port, $client_id);
+        if ($mqtt->connect(true, NULL, $username, $password)) {
+            $mqtt->publish($canal, $msg, 1);
+            $mqtt->close();
+        } else {
+            file_put_contents("debug.output", "Time out!");
+        }
+    }
+
+}
