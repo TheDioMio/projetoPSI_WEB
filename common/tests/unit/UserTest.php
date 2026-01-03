a894b91a9da5f921f222775327d6b258cffebfe2
mariopina <2241534@my.ipleiria.pt>
2026-01-03 14:36:29 +0000

    Testes
    Testes finalizados

--- common/tests/unit/UserTest.php
+++ common/tests/unit/UserTest.php
@@ -41,10 +41,69 @@ class UserTest extends \Codeception\Test\Unit
 
     }
 
-
-        // tests
-    public function testSomeFeature()
+    public function testSaveAndRead()
     {
+        $user = $this->createValidUser(false);
+        $user->name = 'Zacarias';
 
+        $result = $user->save();
+
+        if (!$result) {
+            var_dump($user->errors);
+        }
+
+        $this->assertTrue($result);
+    }
+
+    public function testUpdateAndRead(){
+
+        $user = $this->createValidUser(true);
+
+        $userReadFromDatabase = User::find()->where(['id' => $user->id])->one();
+        $this->assertNotNull($userReadFromDatabase, 'Nao foi encontrada pessoa com nome Maria');
+        $this->assertEquals('userTeste', $userReadFromDatabase->name, 'The name of the person found in Database is different' );
+
+        $user->name = 'Joana';
+        $user->save();
+
+        $userReadFromDatabase2 = User::find()->where(['id' => $user->id])->one();
+        $this->assertEquals('Joana', $userReadFromDatabase2->name, 'The name of the person found in Database is different' );
+    }
+
+    public function testDelete(){
+
+        $user = $this->createValidUser(true);
+
+        $userReadFromDatabase = User::find()->where(['id' => $user->id])->one();
+        $this->assertNotNull($userReadFromDatabase, 'Nao foi encontrada pessoa com nome Maria');
+
+        $userReadFromDatabase->delete();
+        $userReadFromDatabase2 = User::find()->where(['id' => $user->id])->one();
+        $this->assertNull($userReadFromDatabase2, 'A pessoa não foi apagada');
+    }
+
+    private function createValidUser(bool $save = false): User
+    {
+        $user = new User();
+
+        $user->scenario = User::SCENARIO_DEFAULT;
+
+        $user->name = 'userTeste';
+        $user->username = 'user_' . uniqid();
+        $user->email = uniqid() . '@teste.pt';
+
+        $user->role_id = User::ROLE_USER;
+
+        $user->status = User::STATUS_ACTIVE;
+
+        // Campos técnicos
+        $user->setPassword('123456789');
+        $user->generateAuthKey();
+
+        if ($save) {
+            $user->save(false);
+        }
+
+        return $user;
     }
 }
